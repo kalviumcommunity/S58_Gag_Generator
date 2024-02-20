f6710a859c73c5571c273a748a745ee48df3ab15
adhiraj-singh-khubbar <adhiraj.singh@kalvium.community>
2024-02-20 14:22:34 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express=require ("express");
+const app= express();
+const port=8008;
+
+app.get("/ping",(req,res)=>{
+    res.send("Pong")
+})
+
+
+app.listen(port,()=>{
+    console.log (`server is running on ${port}`)
+})
 
